d00e098ae7c13ac259005de02ed28083272fb56e
Atul Singh <iamaksingh11@gmail.com>
2022-02-20 16:04:35 +0530

    nn

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ print("vgjvgjhvjhvjhvvvvvvvvvvvvvvvvvvvvvvv")
 setup(
   name = 'DjangoTemplateConverter',         # How you named your package folder (MyLib)
   packages = ['DjangoTemplateConverter'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'YOUR NAME',                   # Type in your name
@@ -25,10 +25,10 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
-  scripts=['bin/funniest-joke'],
-  entry_points={
-   'console_scripts': [
-        'DjangoTemplateConverter = DjangoTemplateConverter.start:printLine',
-    ],
-  }
+  scripts=['bin/DjangoTemplateConverter-joke'],
+  # entry_points={
+  #  'console_scripts': [
+  #       'DjangoTemplateConverter = DjangoTemplateConverter.start:printLine',
+  #   ],
+  # }
 )
