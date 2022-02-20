02dbe220bbde4f0b2b6ab4af4f2f760aac4c7d7c
Atul Singh <iamaksingh11@gmail.com>
2022-02-20 20:10:41 +0530

    1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'DjangoTemplateConverter',         # How you named your package folder (MyLib)
   packages = ['DjangoTemplateConverter'],   # Chose the same as "name"
-  version = '1.1',      # Start with a small number and increase it with every change you make
+  version = '1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'YOUR NAME',                   # Type in your name
