036c2ff3498003584c241aa3d5d182e660d8be76
Atul Singh <iamaksingh11@gmail.com>
2022-02-20 16:26:57 +0530

    bb

--- DjangoTemplateConverter/Start.py
+++ DjangoTemplateConverter/Start.py
@@ -1,3 +1,17 @@
-def printLine():
+import argparse
+
+
+
+
+
+
+
+def main():
+    import sys
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--arg1', help='arg1 help')
+    parser.add_argument('--arg2', help='arg2 help')
+    args = parser.parse_args()
+    print("arg1 {}, arg2 {}".format(args.arg1, args.arg2))
     print("hello brother")
 
