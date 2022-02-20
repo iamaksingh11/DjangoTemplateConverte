d21ee4bbf727e2a7137fda943546d45baaedf9d7
Atul Singh <iamaksingh11@gmail.com>
2022-02-20 15:30:26 +0530

    dd

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'YOUR NAME',                   # Type in your name
   author_email = 'iamaksingh11@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/iamaksingh11/DjangoTemplateConverter',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/iamaksingh11/DjangoTemplateConverter/archive/refs/tags/v_02.tar.gz',    # I explain this later on
+  # download_url = 'https://github.com/iamaksingh11/DjangoTemplateConverter/archive/refs/tags/v_02.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           # 'validators',
